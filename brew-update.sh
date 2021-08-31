102d7bbdf3d1b243ee0a8f69eaf75026e56aea61
Ted <oancea.tudor@icloud.com>
2021-08-31 14:10:01 +0200

    Now the logs are saved to a new file each day All logs are kept

--- brew-update.sh
+++ brew-update.sh
@@ -1,10 +1,10 @@
 #!/usr/local/bin/zsh
 ZSHRC_FILE="/Users/tudoroancea/.zshrc"
 BREW_UPDATE_DIR="/Users/tudoroancea/Documents/projets/brew-update/"
-BREW_UPDATE_LOG_FILE=$BREW_UPDATE_DIR"log.txt"
 LINE=`grep 'LAST_BREW_UPDATE' $ZSHRC_FILE`
 OLD_DATE=${LINE:24}
 CURRENT_DATE=`date +%d-%m`
+BREW_UPDATE_LOG_FILE=$BREW_UPDATE_DIR"logs/log-"$CURRENT_DATE".txt"
 if [ $OLD_DATE != $CURRENT_DATE ]
 then
     echo "Trying to update Homebrew..."
